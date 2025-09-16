b51e1075987bca5f4879f500fe916ba50be2e6fc
Omid Jaberi <omid.jaberie@gmail.com>
2025-09-16 17:49:07 +0330

    Initial models created

---
+++ internal/domain/models.go
@@ -0,0 +1,47 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
+)
+
+type Currency struct {
+	Code		string
+	Name		string
+	Symbol		string
+	Decimals	int
+}
+
+type Money struct {
+	Amount		decimal.Decimal
+	Currency	Currency
+}
+
+type Asset struct {
+	ID			uuid.UUID
+	UserID		uuid.UUID
+	Name		string
+	Value		Money
+	Category	string
+	CreatedAt	time.Time
+}
+
+type Transaction struct {
+	ID			uuid.UUID
+	UserID		uuid.UUID
+	Description	string
+	Amount		Money
+	Frequency	string
+	StartDate	time.Time
+	EndDate		time.Time
+	CreatedAt	time.Time
+}
+
+type ExchangeRate struct {
+	UserID	uuid.UUID
+	From	string
+	To		string
+	Rate	decimal.Decimal
+}
